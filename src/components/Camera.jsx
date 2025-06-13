712c0d8de3853732d3fcde6e2da1296edbc17ec9
bboyleonp <bboyleonp@gmail.com>
2025-06-13 16:23:12 +0800

    update the css to better

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -7,13 +7,13 @@ export default function Camera() {
 
   useEffect(() => {
     if (ref.current) {
-      ref.current.position.set(0, -20, 20);
+      ref.current.position.set(0, -20, 10);
     }
     
     const gui = new GUI();
     gui.add(ref.current.position, 'x', -50, 50, 1).name('Camera X');
     gui.add(ref.current.position, 'y', -50, 50, 1).name('Camera Y');
-    gui.add(ref.current.position, 'z', 20, 50, 1).name('Camera Z');
+    gui.add(ref.current.position, 'z', 0, 50, 1).name('Camera Z');
     return () => { gui.destroy(); };
   }, []);
 
